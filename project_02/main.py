8f41583a8fa913f2c2aaf83955a3e0064b79fb34
GarotoOuro <software.engineer.ggs@gmail.com>
2023-04-30 17:14:11 -0300

    Created project_02

---
+++ project_02/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def raiz():
+    return {"msg": "FastAPI"}
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    _host = '0.0.0.0'
+    _port = 8000
+    uvicorn.run("main:app", host=_host, port=_port,
+                log_level="info", reload=True)
