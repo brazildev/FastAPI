933abc9d642f93b68a4723a20a23574e89fcddb6
GarotoOuro <software.engineer.ggs@gmail.com>
2023-05-01 20:09:14 -0300

    Update data.py

--- project_02/data.py
+++ project_02/data.py
@@ -15,5 +15,9 @@ from models import Curso
 
 cursos = [
     Curso(id=1, titulo='Titulo 1', aulas=12, horas=15),
-    Curso(id=2, titulo='Titulo 2', aulas=22, horas=25)
+    Curso(id=2, titulo='Titulo 2', aulas=22, horas=25),
+    Curso(id=3, titulo='Titulo 3', aulas=32, horas=35),
+    Curso(id=4, titulo='Titulo 4', aulas=42, horas=45),
+    Curso(id=5, titulo='Titulo 5', aulas=52, horas=55),
+    Curso(id=6, titulo='Titulo 6', aulas=62, horas=65),
 ]
