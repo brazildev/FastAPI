46d80c0dde38547252d907b24ab51a9c01b8b071
GarotoOuro <software.engineer.ggs@gmail.com>
2023-05-01 20:05:40 -0300

    Update data.py

--- project_02/data.py
+++ project_02/data.py
@@ -1,12 +1,19 @@
-cursos = {
-    1: {
-        'titulo': 'Pro',
-        'aulas': 112,
-        'horas': 58
-    },
-    2: {
-        'titulo': 'Algoritmo',
-        'aulas': 87,
-        'horas': 67
-    }
-}
+from models import Curso
+
+# cursos = {
+#     1: {
+#         'titulo': 'Pro',
+#         'aulas': 112,
+#         'horas': 58
+#     },
+#     2: {
+#         'titulo': 'Algoritmo',
+#         'aulas': 87,
+#         'horas': 67
+#     }
+# }
+
+cursos = [
+    Curso(id=1, titulo='Titulo 1', aulas=12, horas=15),
+    Curso(id=2, titulo='Titulo 2', aulas=22, horas=25)
+]
